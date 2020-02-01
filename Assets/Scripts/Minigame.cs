95b80bb192f97344438d37c6f68c765b221235ed
Mike <mbrwaldron@yahoo.com>
2020-02-01 16:58:58 -0500

    added neighbours

--- Assets/Scripts/Minigame.cs
+++ Assets/Scripts/Minigame.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class Minigame : MonoBehaviour
 {
     public float taskDuration;
+    public bool isActive;
 
     [SerializeField]
     protected float currentTaskCompletion; // Number of seconds contributed to task
