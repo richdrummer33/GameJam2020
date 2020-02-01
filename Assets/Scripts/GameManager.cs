5c138d2c71cd62c363a105c8dadfd18279441d16
Dmitrii Kartashev <faithfinder@yandex.ru>
2020-02-01 10:48:39 -0500

    Basic timer in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float countdown;
+    public float targetTime = 60.0f;
+    public List<GameObject> taskList;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        countdown = targetTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f)
+        {
+            countdown = targetTime;
+            countDownFinished();
+        }
+    }
+
+    void countDownFinished()
+    {
+        //Add a prefabed task???
     }
 }
