fe6ea8f021231732a56ffcc92ceb9c8fca9e594f
Andy Maloney <andy@ackackgames.com>
2020-02-01 11:45:09 -0500

    Add basic main menu scene
    - New Game
    - Credits
    - Quit

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnNewGameClicked()
+    {
+        Debug.Log("New Game Cliked");
+    }
+
+    public void OnCreditsClicked()
+    {
+        Debug.Log("Credits Cliked");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
