e496a7214dce0bf91d7e6bcc829538e53ee21c5b
Mike <mbrwaldron@yahoo.com>
2020-02-01 19:06:24 -0500

    tramp addition

---
+++ Assets/Scripts/TrampBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampBehaviour : MonoBehaviour
+{
+    public GameObject playcontroller;
+    private bool triggered;
+    public float bounceforce = 1.0f;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        playcontroller = other.GameObject;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        triggered = false;
+    }
+    void Start()
+    {
+        triggered = false;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggered)
+        {
+            playcontroller.AddForce(transform.up * bounceforce);
+        }
+    }
+}
